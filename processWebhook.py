a6487d7931d87f6a268182a1b8ba0d1be5db81a5
BrandonSandhu <brandonsandhu15@gmail.com>
2022-03-05 18:55:05 +0000

    update

--- processWebhook.py
+++ processWebhook.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import flask
+import os
+import requests
+from flask import send_from_directory
+
+app = flask.Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
+
+@app.route('/')
+@app.route('/home')
+def home():
+	test = requests.get("https://api64.ipify.org/?format=json").text
+	return("IP: " + test)
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
